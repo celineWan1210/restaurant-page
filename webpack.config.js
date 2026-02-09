0900f2036a9c05cba187e1b16bf3c79267c9d339
celineWan1210 <wanyingxuan1210@gmail.com>
2026-02-09 16:19:15 +0800

    Add template and webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,20 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        }),
+    ],
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
 };
