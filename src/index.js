933431c0bfc2deb0689c59db23735a20ab859b16
celineWan1210 <wanyingxuan1210@gmail.com>
2026-02-10 17:59:20 +0800

    Update menu and header style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css"
 import main from "./sections/main-page.js"
 import home from "./sections/home-page.js"
 import contact from "./sections/contact-page.js"
+import menu from "./sections/menu-page.js"
 
 
 // select content
@@ -13,17 +14,30 @@ const welcomePage = document.querySelector(".welcome-page");
 const mainPageDiv = main();
 const homePageDiv = home();
 const contactPageDiv = contact();
+const menuPageDiv = menu();
+
+// select button
+const homeButton = document.querySelector(".home");
+const contactButton = document.querySelector(".contact");
+const menuButton = document.querySelector(".menu");
 
 welcomePage.appendChild(mainPageDiv);
 content.appendChild(homePageDiv);
 
 
 let currentPage = "home";
+homeButton.style.color = "#834333";
 
 document.addEventListener("click", e => {
     if (e.target.classList.contains("contact") && currentPage !== "contact") {
         mainPageDiv.remove();
         homePageDiv.remove();
+        menuPageDiv.remove();
+
+        // change color
+        contactButton.style.color = "#834333";
+        homeButton.style.color = "white";
+        menuButton.style.color = "white";
 
         welcomePage.appendChild(contactPageDiv);
 
@@ -31,10 +45,30 @@ document.addEventListener("click", e => {
     } else if (e.target.classList.contains("home") && currentPage !== "home") {
         mainPageDiv.remove();
         contactPageDiv.remove();
+        menuPageDiv.remove();
+
+        // change color
+        homeButton.style.color = "#834333";
+        contactButton.style.color = "white";
+        menuButton.style.color = "white";
 
         welcomePage.appendChild(mainPageDiv);
         content.appendChild(homePageDiv);
 
         currentPage = "home";
+    } else if (e.target.classList.contains("menu") && currentPage !== "menu") {
+        mainPageDiv.remove();
+        contactPageDiv.remove();
+        homePageDiv.remove();
+
+        // change color
+        menuButton.style.color = "#834333";
+        contactButton.style.color = "white";
+        homeButton.style.color = "white";
+
+        welcomePage.appendChild(menuPageDiv);
+        currentPage = "menu";
     }
 })
+
+
