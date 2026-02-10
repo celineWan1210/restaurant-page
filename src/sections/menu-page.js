a0c11a1936a1f290721bcbb355de76e99687516d
celineWan1210 <wanyingxuan1210@gmail.com>
2026-02-10 17:59:03 +0800

    Update menu and header style

--- src/sections/menu-page.js
+++ src/sections/menu-page.js
@@ -1,6 +1,9 @@
 import latteImage from "../assets/latte-coffee-pic.jpg"
 import americanoImage from "../assets/americano-pic.jpg"
 import cappucinoImage from "../assets/cappucino-pic.jpg"
+import coconutlatteImage from "../assets/coconut-latte.jpg"
+import matchaImage from "../assets/matcha-latte.jpg"
+import milkTeaImage from "../assets/milk-tea.jpg"
 
 export default function menu() {
     const menuDiv = document.createElement("div");
@@ -9,11 +12,13 @@ export default function menu() {
     // build card function
     function buildSectionCard(imageSource, imageAlt, price) {
         const cardSection = document.createElement("div");
+        cardSection.classList.add("card-section")
         const image = document.createElement("img");
         image.src = imageSource;
         image.alt = imageAlt;
 
         const menuDesc = document.createElement("div");
+        menuDesc.classList.add("menu-desc-div");
         const titleP = document.createElement("p");
         const priceP = document.createElement("p");
         titleP.textContent = imageAlt;
@@ -30,9 +35,12 @@ export default function menu() {
 
     const cardOne = buildSectionCard(latteImage, "Coffee latte", "RM12");
     const cardTwo = buildSectionCard(americanoImage, "Americano", "RM8");
-    const cardThree = buildSectionCard(cappucinoImage, "Cappucino", "RM13")
+    const cardThree = buildSectionCard(cappucinoImage, "Cappucino", "RM13");
+    const cardFour = buildSectionCard(coconutlatteImage, "Coconut Coffee latte", "RM14");
+    const cardFive = buildSectionCard(matchaImage, "Matcha latte", "RM12");
+    const cardSix = buildSectionCard(milkTeaImage, "Milk Tea", "RM15");
     
     // append
-    menuDiv.append(cardOne, cardTwo, cardThree);
+    menuDiv.append(cardOne, cardTwo, cardThree, cardFour, cardFive, cardSix);
     return menuDiv;
 }
